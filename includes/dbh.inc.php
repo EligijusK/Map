537bdc217a2fa5439279619b9fd2298fa4dd6912
Eligijus Kiudys <e.kiudys@gmail.com>
2018-08-10 14:29:27 +0300

    Database file updated create tables in datababase

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -9,5 +9,23 @@ $sql = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
 if($sql->connect_error)
 {
     die("Connection failed: " . $conn->connect_error);
+}else
+{
+   $table1 =  "CREATE TABLE users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    password varchar(256),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP)";
+    $table2 =  "CREATE TABLE maps (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Device_Id VARCHAR(191) NOT NULL UNIQUE,
+    Coordinates VARCHAR(191) NOT NULL UNIQUE,
+    Choise varchar(10),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP)";
+    $create1 = mysqli_query($sql, $table1);
+    $create2 = mysqli_query($sql, $table2);
 }
 ?>
